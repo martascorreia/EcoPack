ae9222beccb9313586bf703e6394c510e171d084
Daniel Machado <danielcbmachadopersonal@gmail.com>
2020-11-30 02:00:25 +0000

    location history done, missing opinions tab

---
+++ EcoPack/app/src/main/java/fcul/cm/g20/ecopack/recyclerview/LocationViewHolder.java
@@ -0,0 +1,43 @@
+package fcul.cm.g20.ecopack.recyclerview;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import fcul.cm.g20.ecopack.R;
+
+public class LocationViewHolder extends RecyclerView.ViewHolder {
+    private TextView locationName;
+    private TextView visitDate;
+
+    public LocationViewHolder(@NonNull View itemView, final LocationAdapter.OnLocationClickListener onLocationClickListener) {
+        super(itemView);
+
+        // TODO: ADD THE MARKER SETTER BASED ON THE LOCATION INFO
+        locationName = itemView.findViewById(R.id.item_location_name);
+        visitDate = itemView.findViewById(R.id.item_location_date);
+
+        setDrawingClickListener(itemView, onLocationClickListener);
+    }
+
+    private void setDrawingClickListener(@NonNull View itemView, final LocationAdapter.OnLocationClickListener onLocationClickListener) {
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (onLocationClickListener != null)
+                    if (getAdapterPosition() != RecyclerView.NO_POSITION)
+                        onLocationClickListener.onLocationClick(getAdapterPosition());
+            }
+        });
+    }
+
+    public TextView getLocationName() {
+        return locationName;
+    }
+
+    public TextView getVisitDate() {
+        return visitDate;
+    }
+}
--- EcoPack/app/src/main/java/fcul/cm/g20/ecopack/ui/fragments/ProfileLocationsFragment.java
+++ EcoPack/app/src/main/java/fcul/cm/g20/ecopack/ui/fragments/ProfileLocationsFragment.java
@@ -1,14 +1,21 @@
 package fcul.cm.g20.ecopack.ui.fragments;
 
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.LinkedList;
 
 import fcul.cm.g20.ecopack.R;
+import fcul.cm.g20.ecopack.recyclerview.LocationAdapter;
 
 public class ProfileLocationsFragment extends Fragment {
     public ProfileLocationsFragment() {
@@ -23,6 +30,36 @@ public class ProfileLocationsFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile_info, container, false);
+        View profileLocationsFragmentView = inflater.inflate(R.layout.fragment_profile_locations, container, false);
+
+        RecyclerView recyclerView = profileLocationsFragmentView.findViewById(R.id.locations_container);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        LinkedList<Pair<String, String>> locations = new LinkedList<>();
+        locations.add(new Pair<>("A Loja Ali Do Canto", "30-11-2020, 09:45h"));
+        locations.add(new Pair<>("Strada", "30-11-2020, 09:45h"));
+        locations.add(new Pair<>("Mais Outra Loja", "30-11-2020, 09:45h"));
+        locations.add(new Pair<>("Aqui Cheira a Enxofre", "30-11-2020, 09:45h"));
+        locations.add(new Pair<>("Dados Estáticos São Boring", "30-11-2020, 09:45h"));
+        locations.add(new Pair<>("BTS É Giro De Se Ouvir Nos Workouts", "30-11-2020, 09:45h"));
+        locations.add(new Pair<>("Gastei Demasiado Dinheiro Na Black Friday Da Steam", "30-11-2020, 09:45h"));
+        locations.add(new Pair<>("Acho Que Já Chega Tantas Locations", "30-11-2020, 09:45h"));
+        locations.add(new Pair<>("Acho Que Já Chega Tantas Locations 2", "30-11-2020, 09:45h"));
+        locations.add(new Pair<>("Acho Que Já Chega Tantas Locations 3", "30-11-2020, 09:45h"));
+
+        LocationAdapter locationAdapter = new LocationAdapter(locations);
+        recyclerView.setAdapter(locationAdapter);
+
+        locationAdapter.setOnLocationClickListener(new LocationAdapter.OnLocationClickListener() {
+            @Override
+            public void onLocationClick(int position) {
+                Toast t = Toast.makeText(getContext(), "Location " + position, Toast.LENGTH_SHORT);
+                t.show();
+
+                // TODO: ADD NEW MAPFRAGMENT(COORDINATES) IN ORDER TO GO STRAIGHT AWAY TO THE LOCATION WITH THESE COORDINATES
+            }
+        });
+
+        return profileLocationsFragmentView;
     }
 }
